366ce4d3637116c93bdb6cc8a2379ba67f4c2caa
Kolomiiets Lidiia <kolomieclidia7@gmail.com>
2023-07-18 16:22:49 +0300

    fix task

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const countButton = 6;
 
-//створюємо кнопки
 for (let i = 1; i <= countButton; i++) {
     const btnElement = document.createElement('button');
     btnElement.textContent = i;
@@ -9,49 +8,27 @@ for (let i = 1; i <= countButton; i++) {
 
 const buttonEl = [...document.querySelectorAll('button')];
 
-let pressed = false;
-// Функція, яка перевіряє, чи всі непарні або парні кнопки були натиснуті
-function checkButtons() {
-    const promise = new Promise((resolve) => {
-
-        let pressedPairButton = [];
-        let pressedOddButton = [];
-
-        buttonEl.forEach((button, number) => {
-
-            if (number % 2 === 0 && button.dataset.pressed === "true") {
-                pressedPairButton.push(button);
-            }
-            else if (number % 2 !== 0 && button.dataset.pressed === "true") {
-                pressedOddButton.push(button)
-            }
-        });
-
-        if (pressedOddButton.length === 3 && pressedPairButton.length === 3) {
-            resolve("Натиснуті всі кнопки");
-        }
-        else if (pressedPairButton.length === 3 && pressed === false) {
-            pressed = true;
-            resolve("Натиснуті тільки непарні кнопки.");
-        }
-        else if (pressedOddButton.length === 3 && pressed === false) {
-            pressed = true;
-            resolve("Натиснуті тільки парні кнопки.");
-        }
-
-    })
-    promise.then((value) => {
-        alert(value);
-    })
-    return promise
-}
-
-
-buttonEl.forEach(btnElement => {
+const buttonPromise = buttonEl.map(btnElement => new Promise(resolve => {
     btnElement.onclick = () => {
         btnElement.classList.add("shadow");
-        btnElement.dataset.pressed = "true";
-
-        checkButtons();
+        resolve(+btnElement.textContent)
     }
+}))
+
+const pairedButtons = buttonPromise.filter((_, index) => index % 2 === 1);
+
+const oddButtons = buttonPromise.filter((_, index) => index % 2 !== 1);
+
+Promise.all(pairedButtons).then(() => {
+    console.log('Натиснуті всі парні кнопки')
+
+})
+
+Promise.all(oddButtons).then(() => {
+    console.log('Натиснуті всі непарні кнопки')
+
+})
+Promise.all(buttonPromise).then(() => {
+    console.log('Всі кнопки натиснуті')
+
 })
